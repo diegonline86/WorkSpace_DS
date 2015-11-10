a5f8ecbf72ee185271fdcf29731245307f17a6f8
diegonline86 <diegosilguero@yahoo.es>
2015-11-10 13:08:37 +0100

    proyecto weather dam

---
+++ Proyecto_WeatherDAM/WeatherDAM/app/src/main/java/com/salesianostriana/proyectoconjunto/weatherdam/WeatherMainActivity.java
@@ -0,0 +1,48 @@
+package com.salesianostriana.proyectoconjunto.weatherdam;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class WeatherMainActivity extends AppCompatActivity {
+    RecyclerView rv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_main);
+
+        RecyclerView rv = (RecyclerView)findViewById(R.id.my_recycler_view);
+        rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
